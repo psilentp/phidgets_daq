392c7d0ba1ad80db982e806a83b3e6285243ade1
psilentp <thlindsay1@gmail.com>
2017-04-04 18:37:55 -0700

    modified interpreter functions to record pulse from chrimson led and publish in V

--- demo/phidgets_daq_interpreter_functions.py
+++ demo/phidgets_daq_interpreter_functions.py
@@ -5,20 +5,20 @@ class DAQInterpreter(object):
         self.channel_names = {  0: 'clock',
                                 1: 'lmr',
                                 2: 'freq',
-				3: 'lpr',
-				4: 'empty',
-				5: 'xpos',
-				6: 'ypos',
+				                3: 'lpr',
+				                4: 'chrimson',
+				                5: 'xpos',
+				                6: 'ypos',
                               }
         
-        self.channel_param = { 'clock': {'min': 0, 'max': 5},
-                               'lmr': {'min': 0, 'max': 5},
-			       'lpr': {'min': 0, 'max': 5},
-			       'freq': {'min': 0, 'max': 5},
-                               'empty': {'min': 0, 'max': 5},
-			       'xpos': {'min': 0, 'max': 5},
-			       'ypos': {'min': 0, 'max': 5},
-                                }
+        self.channel_param = { 'clock':    {'min': 0, 'max': 5},
+                               'lmr':      {'min': 0, 'max': 5},
+			                   'lpr':      {'min': 0, 'max': 5},
+			                   'freq':     {'min': 0, 'max': 5},
+                               'chrimson': {'min': -10, 'max': 10},
+			                   'xpos':     {'min': -10, 'max': 10},
+			                   'ypos':     {'min': -10, 'max': 10},
+                             }
 	self.maxt = 30
 
     def interpret_channel(self, channel, value):
@@ -43,14 +43,14 @@ class DAQInterpreter(object):
         actual_value = (raw_value/2.)   
         return actual_value 
     
-    def empty(self, raw_value):
-        actual_value = (raw_value)   
+    def chrimson(self, raw_value):
+        actual_value = ((raw_value-499)*(10.0/136))  
         return actual_value 
     
     def xpos(self, raw_value):
-        actual_value = (raw_value)    
+        actual_value = ((raw_value-499)*(10.0/136))
         return actual_value 
     
     def ypos(self, raw_value):
-        actual_value = (raw_value)    
+        actual_value = ((raw_value-499)*(10.0/136))
         return actual_value 
